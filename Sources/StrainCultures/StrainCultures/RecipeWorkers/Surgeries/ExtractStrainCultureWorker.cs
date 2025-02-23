d655eea5b35865abdd28f7ecfe64fda8ad3f9725
Zeracronius <zeracronius@outlook.dk>
2025-02-23 15:02:25 +0100

    Started work on basic surgeries

---
+++ Sources/StrainCultures/StrainCultures/RecipeWorkers/Surgeries/ExtractStrainCultureWorker.cs
@@ -0,0 +1,14 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace StrainCultures.RecipeWorkers.Surgeries
+{
+	internal class ExtractStrainCultureWorker : Recipe_Surgery
+	{
+	}
+}
---
+++ Sources/StrainCultures/StrainCultures/RecipeWorkers/Surgeries/InjectStrainCultureWorker.cs
@@ -0,0 +1,28 @@
+﻿using RimWorld;
+using StrainCultures.Hediffs;
+using StrainCultures.Things;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace StrainCultures.RecipeWorkers.Surgeries
+{
+	internal class InjectStrainCultureWorker : Recipe_Surgery
+	{
+		public override void ApplyOnPawn(Pawn pawn, BodyPartRecord part, Pawn billDoer, List<Thing> ingredients, Bill bill)
+		{
+			StrainCulture culture = ingredients.OfType<StrainCulture>().FirstOrDefault();
+			if (culture == null)
+			{
+				Mod.Logging.Warning($"Attempted to inject pawn '{pawn.Name}' with strain culture, but no culture was found in the ingredients.");
+				return;
+			}
+
+			pawn.health.AddHediff(new Infection(culture));
+			culture.DeSpawn();
+		}
+	}
+}
