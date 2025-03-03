a713dd38ce60f9f1ab1bb5030a73848af11ea795
Zeracronius <zeracronius@outlook.dk>
2025-03-03 19:47:02 +0100

    Added event scheduler

---
+++ Sources/StrainCultures/Scheduling/IEventHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrainCultures.Scheduling
+{
+	public interface IEventHandler
+	{
+		void HandleEvent(string? signal);
+	}
+}
